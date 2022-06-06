df1d1bde84c4ea8e528dcf53b746e267cb94bea3
Majito♥ <89418133+majito02@users.noreply.github.com>
2022-06-06 13:56:11 -0500

    CREACIÓN DE VARIABLES PARA LOS BOTONES E INFORMACIÒN DEL PERSONAL

--- js/especialistas.js
+++ js/especialistas.js
@@ -5,3 +5,42 @@ const titulo = "Acerca de Blue Monkey Clinic";
 const subt1 = "BLUE MONKEY CLINIC:";
 const cont1 = "HOSPITAL GENERAL PRIVADO SANTO DOMINGO DE LOS TSÁCHILAS";
 const texto = "Los mejores servicios de salud en prevención, rehabilitación y recuperación con seguridad, eficacia y calidez. Somos líderes en servicios de salud certificados con excelencia profesional y tecnología moderna";
+//Variables para botones
+//nombre de botones
+const botones = ['ESPECIALISTAS ♥','FARMACOTERAPIA ♥','PSICÓLOGOS ♥','FISIOTERAPEUTAS ♥'];
+//informacion de personal
+const especialistas = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
+      {"img":"img/Trabajos/2.png","nombre":"Dayana Navarro","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Leonardo Cruz","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Carlos Curiel","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Steven Varela","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Milena Viveres","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Kevin Aranda","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Melany Cueva","categoria":"Psiquiatría"}];
+      
+const farmacoterapia = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
+      {"img":"img/Trabajos/2.png","nombre":"Maria Párraga","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Vinicio Borja","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Carlos Enriquez","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Eduardo Valencia","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Kelly Gomez","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Elian Gualpa","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Carmen Ruiz","categoria":"Psiquiatría"}];
+
+const psicologos = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
+      {"img":"img/Trabajos/2.png","nombre":"Luisa Palma","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Eduardo Moreira","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Pablo Moreira","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Edison Romero","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Marilyn Santos","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Luis Párraga","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Camila Párraga","categoria":"Psiquiatría"}];
+      
+const fisioterapeutas = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
+      {"img":"img/Trabajos/2.png","nombre":"Maribel Moreira","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Fernando Burgos","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Kevin Paute","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Luis Gomez","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Jonsu Burgos","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Katerine Lopez","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Selena Rivas","categoria":"Psiquiatría"}];
