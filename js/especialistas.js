7bab283c722667ec77abe5b60b0b007a9466893b
Majito♥ <89418133+majito02@users.noreply.github.com>
2022-06-06 13:54:55 -0500

    CREACIÓN DE VARIABLES PARA LA SECCIÓN DE DESCRIPCIÓN

---
+++ js/especialistas.js
@@ -0,0 +1,7 @@
+//Variables a editar
+//Variables para seccion de descripcion
+const icono = "img/icono-clin.png" ;
+const titulo = "Acerca de Blue Monkey Clinic";
+const subt1 = "BLUE MONKEY CLINIC:";
+const cont1 = "HOSPITAL GENERAL PRIVADO SANTO DOMINGO DE LOS TSÁCHILAS";
+const texto = "Los mejores servicios de salud en prevención, rehabilitación y recuperación con seguridad, eficacia y calidez. Somos líderes en servicios de salud certificados con excelencia profesional y tecnología moderna";
